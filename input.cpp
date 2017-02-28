a2e594d105d1780272b6897c8311973aec8beb46
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-28 14:26:32 +0900

    input

---
+++ input.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string text = "Hello";
+
+  cout << text << endl;
+
+  cout << "Enter you name: " << flush;
+  string input;
+
+  cin >> input;
+
+  cout << "You entered: " << input << endl;
+  int value;
+  cin >> value;
+  cout << "You entered: " << value << endl;
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
