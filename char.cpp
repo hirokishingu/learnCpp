4f566ef99be742c9c77cbccfa346fed9b18ee37b
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-28 16:28:44 +0900

    floatの数字の扱いに注意

---
+++ char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  bool bValue = true;
+  cout << bValue << endl;
+  char cValue = '7';
+  cout << cValue << endl;
+
+  wchar_t wValue = 'i';
+  cout << (char)wValue << endl;
+  return 0;
+}
