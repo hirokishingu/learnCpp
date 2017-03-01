7024f6378b3bb6047e2309ab64c6835fba6dfcf7
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-03-01 10:16:48 +0900

    get set start

---
+++ classdemo/Cat.h
@@ -0,0 +1,19 @@
+#ifndef CAT_H_
+#define CAT_H_
+
+class Cat {
+private:
+  bool happy;
+
+public:
+  void speak();
+  Cat();
+  ~Cat();
+};
+
+#endif /* CAT_H_ */
+
+
+
+
+
--- get start/for.cpp
+++ get start/for.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-
-
+  for(int i=0; i<10; i++){
+    cout << "Hello " << i << endl;
+  }
   return 0;
 }
