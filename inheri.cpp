9e46b9feff2e752ee7871d2758bac9550cb78b0d
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-03-02 19:50:34 +0900

    inheritant

---
+++ inheri.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+  void speak() { cout << "Grrrr" << endl; }
+};
+
+class Cat: public Animal{
+public:
+  void jump() { cout << "Cat jumping!" << endl; }
+};
+
+class Tiger: public Cat {
+public:
+  void attackAntelope(){ cout << "Attacking!" << endl; }
+};
+
+int main(){
+
+  Animal a;
+  a.speak();
+
+  Cat cat;
+  cat.speak();
+  cat.jump();
+  Tiger tiger;
+  tiger.attackAntelope();
+
+
+  return 0;
+}
+
+
+
